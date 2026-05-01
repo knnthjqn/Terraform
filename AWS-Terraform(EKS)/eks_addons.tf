3ebdf00c1f8d566fae9c632fc0482d2d7c855bfa
knnthjqn <kennethjoaquin0@gmail.com>
2026-05-01 16:45:18 +0800

    Add files via upload

---
+++ AWS-Terraform(EKS)/eks_addons.tf
@@ -0,0 +1,19 @@
+resource "aws_eks_addon" "vpc_cni" {
+    cluster_name = aws_eks_cluster.main.name
+    addon_name = "vpc-cni"
+}
+
+resource "aws_eks_addon" "coredns" {
+    cluster_name = aws_eks_cluster.main.name
+    addon_name = "coredns"
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+    cluster_name = aws_eks_cluster.main.name
+    addon_name = "kube_proxy"
+}
+
+resource "aws_eks_addon" "pod_identity_agent" {
+    cluster_nane = aws_eks_cluster.main.name
+    addon_name = "eks-pod-identity-agent"
+}
