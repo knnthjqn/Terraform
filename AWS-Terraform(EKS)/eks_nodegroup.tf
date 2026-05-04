ec651341469c2ce1881152d02472e6931c34ffc5
knnthjqn <kennethjoaquin0@gmail.com>
2026-05-04 22:12:07 +0800

    Fix resource declaration for EKS node group

--- AWS-Terraform(EKS)/eks_nodegroup.tf
+++ AWS-Terraform(EKS)/eks_nodegroup.tf
@@ -51,9 +51,9 @@ resource "aws_eks_note_group" "general" {
         max_size = 4
     }
 
-update_config {
-    max_unavailable = 1
-}
+    update_config {
+        max_unavailable = 1
+    }
 
     depends_on = [
         aws_iam_role_policy_attachment.eks_worker_nodes,
