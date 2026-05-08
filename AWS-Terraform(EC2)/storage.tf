2e48bb01f60323ff8e6ecdad3b732d6c70a40b03
knnthjqn <kennethjoaquin0@gmail.com>
2026-05-09 04:34:24 +0800

    Refactor S3 bucket resources for media storage

--- AWS-Terraform(EC2)/storage.tf
+++ AWS-Terraform(EC2)/storage.tf
@@ -1,35 +1,3 @@
-resource "aws_s3_bucket" "logs" {
-    bucket = "${var.project_name}-${var.environment}-logs--bucket"
-}
-
-resource "aws_s3_bucket_versioning" "logs" {
-    bucket = aws_s3_bucket.logs.id
-
-    versioning_configuration { 
-        enabled = true
-    }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
-    bucket = aws_s3_bucket.logs.id
-
-    rule {
-        apply_server_side_encryption_by_default {
-            kms_key_id = aws_kms_key.main.arn
-            sse_algorithm = "aws:kms"
-        }
-    }
-}
-
-resource "aws_s3_bucket_public_access_block" "logs" {
-    bucket = aws_s3_bucket.logs.id
-
-    block_public_acls = true
-    block_public_policy = true
-    ignore_public_acls = true
-    restrict_public_buckets = true
-}
-
 resource "aws_s3_bucket" "media" {
     bucket = "${var.project_name}-${var.environment}-media-bucket"
 }
@@ -38,7 +6,7 @@ resource "aws_s3_bucket_versioning" "media" {
     bucket = aws_s3_bucket.media.id
 
     versioning_configuration {
-        enabled = true
+        status = "Enabled"
     }
 }
 
