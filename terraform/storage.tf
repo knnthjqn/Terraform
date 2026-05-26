f2a9e1be74bbd2d778a2f40924482165e1e559f2
knnthjqn <kennethjoaquin0@gmail.com>
2026-05-26 11:03:09 +0800

    Refactor S3 bucket resources for media and deploy artifacts

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "media" {
-    bucket = "${var.project_name}-media-bucket-${data.aws_caller_identity.current.id}
+    bucket = "${var.project_name}-media-bucket-${data.aws_caller_identity.current.id}"
 }
 
 resource "aws_s3_bucket_versioning" "media" {
