afcaa3cc2d7eadf7a6f310a305241fa2fb3b668c
knnthjqn <kennethjoaquin0@gmail.com>
2026-05-26 12:21:16 +0800

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,21 +1,21 @@
 resource "aws_launch_template" "web" {
-    name = "${var.project_name}-web-lt"
-    image_id = data.aws_ami.al2023.id
-    instance_type = "t3.micro"
+  name = "${var.project_name}-${var.environment}-web-"
+  image_id = data.aws_ami.al2023.id
+  instance_type = var.web_instance_type
 
-    iam_instance_profile {
-        name = aws_iam_instance_profile.web.name
-    }
+  iam_instance_profile {
+    name = aws_iam_instance_profile.web.name
+  }
 
-    vpc_security_group_id = aws_security_group.web.id
+  vpc_security_group_ids = [aws_security_group.web.id]
 
-    user_data = base64encode(<<-EOF
-        #!/bin/bash
-        dnf update -y
-        dnf install -y nginx
-        systemctl enable nginx
-        systemctl start nginx
-    EOF)
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    dnf update -y
+    dnf install -y nginx
+    systemctl enable nginx
+    systemctl start nginx
+  EOF)
 }
 
 resource "aws_launch_template" "app" {
