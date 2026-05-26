e8345f731977d2e7d12bc896d4859f8de916b614
knnthjqn <kennethjoaquin0@gmail.com>
2026-05-26 11:00:03 +0800

    Refactor IAM roles and policies for web and app

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,52 +1,59 @@
 resource "aws_iam_role" "web" {
-    name = "${var.project_name}-${var.environment}-web-role"
+    name = "${var.project_name}-web-role"
 
     assume_role_policy = jsonencode({
+        Version = "2012-10-17"
         Statement = [{
             Effect = "Allow",
-            Resource = "ec2.amazonaws.com",
+            Principal = {
+                Service = "ec2.amazonaws.com"
+            },
             Action = "sts:AssumeRole"
         }]
     })
 }
 
-resource "aws_iam_instance_profile" {
-    name = "${var.project_name}-${var.environment}-web-profile"
-    role = aws_iam_role.web.name
+resource "aws_iam_instance_profile" "web" {
+    name = "${var.project_name}-web-profile"
+    role = aws_iam_role.web.id
 }
 
-resource "aws_iam_role_policy_attachment" "web" {
-    role = aws_iam_role.web.name
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMManagedInstanceCore"    
+resource "aws_iam_role_policy_attachmennt" "web_smm" {
+    role = aws_iam_role.web.id
+    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_role" "app" {
-    name = "${var.project_name}-${var.environment}-app-role"
+    name = "${var.project_name}-app-role"
 
     assume_role_policy = jsonencode({
+        Version = "2012-10-17"
         Statement = [{
             Effect = "Allow",
-            Resource = "ec2.amazonaws.com",
+            Principal = {
+                Service = "ec2.amazonaws.com"
+            },
             Action = "sts:AssumeRole"
         }]
     })
 }
 
 resource "aws_iam_instance_profile" "app" {
-    name = "${var.project_name}-${var.environment}-app-profile"
-    role = aws_iam_role.app.name
+    name = "${var.project_name}-app-profile"
+    role = aws_iam_role.app.id
 }
 
-resource "aws_iam_role_policy_attachment" "app" {
-    role = aws_iam_role.app.name
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMManagedInstanceCore"
+resource "aws_iam_role_policy_attachment" "app_ssm" {
+    role = aws_iam_role.app.id
+    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-resource "aws_iam_role_policy" "app" {
-    name = "${var.project_name}-${var.environment}-app-inline"
-    role = aws_iam_role.app.name
+resource "aws_iam_role_policy" "app_inline" {
+    name = "${var.project_name}-app-inline-policy"
+    role = aws_iam_role.app.id
 
     policy = jsonencode({
+        Version = "2012-10-17"
         Statement = [
             {
                 Effect = "Allow",
@@ -64,7 +71,7 @@ resource "aws_iam_role_policy" "app" {
                 Effect = "Allow",
                 Actions = [
                     "secretsmanager:GetSecretValue",
-                    "secretsmanager:DescribeSecret"
+                    "secretsmanage:DescribeSecret"
                 ],
                 Resource = aws_secretsmanager_secret.db.arn
             },
@@ -90,15 +97,15 @@ resource "aws_iam_role_policy" "app" {
     })
 }
 
-
 resource "aws_iam_role" "rds_proxy" {
-    name = "${var.project_name}-${var.environment}-rds-assume-role"
+    name = "${var.project_name}-proxy-role"
 
     assume_role_policy = jsonencode({
+        Version = "2012-10-17"
         Statement = [{
             Effect = "Allow",
-            Resource = {
-                Service = "rds.amazonaws.com",
+            Principal = {
+                Service = "rds.amazonaws.com"
             },
             Action = "sts:AssumeRole"
         }]
@@ -106,20 +113,97 @@ resource "aws_iam_role" "rds_proxy" {
 }
 
 resource "aws_iam_role_policy" "rds_proxy" {
-    name = "${var.project_name}-${var.environment}-rds-role-policy"
-    role = aws_iam_role.app.name
+    name = "${var.project_name}-proxy-policy"
+    role = aws_iam_role.rds_proxy.id
 
     policy = jsonencode({
+        Version = "2012-10-17"
         Statement = [
             {
                 Effect = "Allow",
-                Action = "sceretsmanager:GetSecretValue",
+                Actions = [
+                    "secretsmanager:GetSecretValue"
+                ],
                 Resource = aws_secretsmanager_secret.db.arn
             },
             {
                 Effect = "Allow",
-                Action = "kmsDecrypt",
-                Resource = aws_kms_key.main.arn
+                Actions = [
+                    "kms:Decrypt"
+                ],
+                Resource = aws_kms_key.main.id
+            }
+        ]
+    })
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+    url = "https://token.actions.githubusercontent.com"
+    client_id_list = ["sts.amazonaws.com"]
+    thumbprint_list = var.github_oidc_thumbprint_list
+}
+
+resource "aws_iam_role" "github_actions" {
+    name = "${var.project_name}-github-actions-role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+            Effect = "Allow",
+            Principal = {
+                Federated = var.aws_iam_openid_connect_provider.github.arn
+            },
+            Action = "sts:AssumeRoleWithWebIdentity"
+        }]
+    })
+}
+
+resource "aws_iam_role_policy" "github_actions" {
+    name = "${var.project_name}-github-actions-policy"
+    role = aws_iam_role.github_actions.id
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow",
+                Actions = [
+                    "s3:GetObject",
+                    "s3:PutObject",
+                    "s3:ListBucket"
+                ],
+                Resources = [
+                    aws_s3_bucket.media.arn,
+                    "${aws_s3_bucket.media.arn}/*"
+                ]
+            },
+            {
+                Effect = "Allow",
+                Actions = [
+                    "kms:Encrypt",
+                    "kms:Decrypt",
+                    "kms:GenerateDataKey"
+                ],
+                Resource = awS_kms_key.main.arn
+            },
+            {
+                Effect = "Allow",
+                Actions = [
+                    "ssm:SendCommand",
+                    "ssm:GetCommandInvocation",
+                    "ssm:ListCommandInvocation",
+                    "ssm:DescribeInstanceInformation"
+                ],
+                Resource = "*"
+            },
+            {
+                Effect = "Allow",
+                Actions = [
+                    "ec2:DescribeInstances",
+                    "autoscaling:DescribeAutoScalingGroups",
+                    "elasticloadbalancing:DescribeLoadBalancers"
+                ],
+                Resource = "*"
             }
         ]
     })
