fadc936e4460752fbd4d4835fc522b6d2b701ca9
knnthjqn <kennethjoaquin0@gmail.com>
2026-05-26 11:02:55 +0800

    Update storage.tf

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "media" {
-    bucket = "${var.project_name}-${var.environment}-media-bucket"
+    bucket = "${var.project_name}-media-bucket-${data.aws_caller_identity.current.id}
 }
 
 resource "aws_s3_bucket_versioning" "media" {
     bucket = aws_s3_bucket.media.id
-    
+
     versioning_configuration {
         status = "Enabled"
     }
@@ -21,11 +21,43 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "media" {
     }
 }
 
-resource "aws_s3_bucket_pubic_access_block" "media" {
+resource "aws_s3_bucket_public_access_block" "media" {
     bucket = aws_s3_bucket.media.id
 
     block_public_acls = true
     block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "deploy_artifacts" {
+    bucket = "${var.project_name}-${environment}-deploy-artifacts-${data.aws_caller_identity.current.id}
+}
+
+resource "aws_s3_bucket_versioning" "deploy_artifacts" {
+    bucket = aws_s3_bucket.deploy_artifacts.id
+
+    versioning_configuration {
+        status = "Enabled"
+    }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "deploy_artifacts" {
+    bucket = aws_s3_bucket.deploy_artifacts.id
+
+    rule {
+        apply_server_side_encryption_by_default {
+            kms_key_id = aws_kms_key.main.id
+            sse_algorithm = "aws:kms"
+        }
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "deploy_artifacts" {
+    bucket = aws_s3_bucket.deploy_artifacts.id
+
+    block_public_policy = true
+    block_public_acls = true
     ignore_public_acls = true
     restrict_public_buckets = true
 }
