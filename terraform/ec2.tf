c5c57c333dc24a07ad9a50f235846fc84928a94b
knnthjqn <kennethjoaquin0@gmail.com>
2026-05-26 12:24:13 +0800

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -17,7 +17,8 @@ resource "aws_launch_template" "web" {
     dnf install -y nginx
     systemctl enable nginx
     systemctl start nginx
-  EOF)
+  EOF
+  )
 }
 
 resource "aws_launch_template" "app" {
