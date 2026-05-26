fadfda0f7f8dbad3a1320e568b67877c2c791d77
knnthjqn <kennethjoaquin0@gmail.com>
2026-05-26 11:00:41 +0800

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-    cidr_block = var.vpc_cidr
+    cidr_block = "${var.vpc_cidr}
     enable_dns_hostname = true
     enable_dns_support = true
 }
@@ -15,18 +15,39 @@ resource "aws_subnet" "public" {
     cidr_block = each.value
     availability_zones = local.azs[index(keys(local.public_subnets), each.key)]
     map_public_ip_on_launch = true
+
+    tags = {
+        name = "${var.project_name}-public-subnets"
+    }
 }
 
 resource "aws_subnet" "private" {
-    for_each = local.private_subnets 
+    for_each = local.private_subnets
 
     vpc_id = aws_vpc.main.id
     cidr_block = each.value
     availability_zones = local.azs[index(keys(local.private_subnets), each.key)]
+
+    tags = {
+        name = "${var.project_name}-private-subnets"
+    }
+}
+
+resource "aws_subnet" "data" {
+    for_each = local.data_subnets
+
+    vpc_id = aws_vpc.main.id
+    cidr_block = each.value
+    availability_zones = local.azs[index(keys(local.data_subnets), each.key)]
+
+    tags = {
+        name = "${var.project_name}-data-subnets"
+    }
 }
 
 resource "aws_eip" "nat" {
     for_each = aws_subnet.public
+
     domain = "vpc"
 }
 
@@ -34,7 +55,9 @@ resource "aws_nat_gateway" "main" {
     for_each = aws_subnet.public
 
     subnet_id = each.value.id
-    allocation_id = aws_eip.main[each.key].id
+    allocation_id = aws_eip.nat[each.key].id
+
+    depends_on = [aws_internet_gateway.main]
 }
 
 resource "aws_route_table" "public" {
@@ -56,6 +79,10 @@ resource "aws_route_table" "private" {
     }
 }
 
+resource "aws_route_table" "data" {
+    vpc_id = aws_vpc.main.id
+}
+
 resource "aws_route_table_association" "public" {
     for_each = aws_subnet.public
 
@@ -64,8 +91,15 @@ resource "aws_route_table_association" "public" {
 }
 
 resource "aws_route_table_association" "private" {
-    for_each = aws_subnet.private
+    for_each = aws_subnet.private 
 
     subnet_id = each.value.id
     route_table_id = aws_route_table.private[each.key].id
+}
+
+resource "aws_route_table_association" "data" {
+    for_each = aws_subnet.data
+
+    subnet_id = each.value.id
+    route_table_id = aws_route_table.data.id
 }
